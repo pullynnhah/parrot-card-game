28fc2b94858227033c55f9ec8330fa3b18af640d
Paula Rodrigues <pullynnhah@icloud.com>
2023-03-15 18:04:13 -0300

    :sparkles: implement function for getting number of cards from user

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello!");
+function getDeckSize() {
+  let size = NaN;
+  while (!isDeckSizeValid(size, MIN_CARDS, MAX_CARDS)) {
+    size = Number(
+      prompt(`Com quantas cartas deseja jogar?\nNúmero PAR entre [${MIN_CARDS} - ${MAX_CARDS}]`)
+    );
+  }
+
+  return size;
+}
+
+const PARROTS = ["quad", "imposter", "tiedye", "christmas", "darkmode", "twins", "brazilianplayer"];
+const MIN_CARDS = 4;
+const MAX_CARDS = 14;
+
+getDeckSize();
